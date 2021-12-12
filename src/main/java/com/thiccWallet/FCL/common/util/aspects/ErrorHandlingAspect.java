98da0a736fbc077a7802c164a8e6da05b5e19795
Andrew Aslakson <andrew01177@gmail.com>
2021-12-12 16:53:21 -0500

    added log-in/log-out in login controller and necessary UserService Methods, testing now

--- src/main/java/com/thiccWallet/FCL/common/util/aspects/ErrorHandlingAspect.java
+++ src/main/java/com/thiccWallet/FCL/common/util/aspects/ErrorHandlingAspect.java
@@ -3,6 +3,8 @@ package com.thiccWallet.FCL.common.util.aspects;
 import com.thiccWallet.FCL.common.dtos.ErrorResponse;
 import com.thiccWallet.FCL.common.exception.DuplicateCredentialsException;
 import com.thiccWallet.FCL.common.exception.InvalidRequestException;
+import com.thiccWallet.FCL.common.exception.NoSuchUserException;
+import com.thiccWallet.FCL.common.exception.NotLoggedInException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,5 +19,18 @@ public class ErrorHandlingAspect {
     public ErrorResponse handleBadRequests(Exception e) {
         return new ErrorResponse(400, e);
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NoSuchUserException.class})
+    public ErrorResponse handleNotFoundRequests(Exception e) {
+        return new ErrorResponse(404, e);
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler({NotLoggedInException.class})
+    public ErrorResponse handleUnauthorizedRequests(Exception e) {
+        return new ErrorResponse(401, e);
+    }
+
 
 }
