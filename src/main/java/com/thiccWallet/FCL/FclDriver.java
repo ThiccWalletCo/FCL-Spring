4306ab9720ffef8abedba65217be878da1be8a3a
Cole Paris <coleparis2021@gmail.com>
2021-12-15 15:02:33 -0800

    coinbase_dao

--- src/main/java/com/thiccWallet/FCL/FclDriver.java
+++ src/main/java/com/thiccWallet/FCL/FclDriver.java
@@ -1,5 +1,6 @@
 package com.thiccWallet.FCL;
 
+import com.thiccWallet.FCL.data.coin.CoinbaseDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
