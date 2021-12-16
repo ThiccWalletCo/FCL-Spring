0a502564a9ff9a0009786fa966ce7082522b511a
Michael Chau <82185884+Sudocodo@users.noreply.github.com>
2021-12-16 08:14:51 -0800

    Removed unused import

--- src/main/java/com/thiccWallet/FCL/FclDriver.java
+++ src/main/java/com/thiccWallet/FCL/FclDriver.java
@@ -1,6 +1,5 @@
 package com.thiccWallet.FCL;
 
-import com.thiccWallet.FCL.data.coin.CoinbaseDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
