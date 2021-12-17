5e558434b2296612070d167bd27f4187ec4d84c9
Cole Paris <coleparis2021@gmail.com>
2021-12-17 08:13:28 -0800

    Coin service, dto, exception & controller updates

--- src/main/java/com/thiccWallet/FCL/data/coin/CoinController.java
+++ src/main/java/com/thiccWallet/FCL/data/coin/CoinController.java
@@ -1,7 +1,15 @@
 package com.thiccWallet.FCL.data.coin;
 
+import com.thiccWallet.FCL.common.exception.NoWalletException;
+import com.thiccWallet.FCL.common.exception.NotLoggedInException;
+import com.thiccWallet.FCL.data.coin.dtos.requests.CoinPurchaseRequest;
+import com.thiccWallet.FCL.endpoints.wallets.Wallet;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -28,4 +36,23 @@ public class CoinController {
     public List<Coin> getCoinsByWallet(@PathVariable String walletId) {
         return coinService.getCoinsByWallet(walletId);
     }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/buy", consumes = "application/json")
+    public void buyCoin(@RequestBody CoinPurchaseRequest buyCoin, HttpServletRequest req){
+        HttpSession session = req.getSession(false);
+        if (session == null) throw new NotLoggedInException("Cannot buy coin. You're not logged in!.");
+        Wallet wallet = (Wallet) session.getAttribute("currentWallet");
+        if(wallet == null) {
+            throw new NoWalletException("User has not selected a current wallet.");
+        }
+        if(coinService.validBuyAmount(wallet, buyCoin)){
+
+        }
+    }
+
+    //insert coins method
+
+
+    //update coins
+
 }
--- src/main/java/com/thiccWallet/FCL/data/coin/CoinService.java
+++ src/main/java/com/thiccWallet/FCL/data/coin/CoinService.java
@@ -1,21 +1,29 @@
 package com.thiccWallet.FCL.data.coin;
 
+import com.thiccWallet.FCL.common.exception.InsufficientFundsException;
+import com.thiccWallet.FCL.data.coin.dtos.requests.CoinPurchaseRequest;
 import com.thiccWallet.FCL.endpoints.users.User;
 import com.thiccWallet.FCL.endpoints.users.dtos.responses.UserResponse;
+import com.thiccWallet.FCL.endpoints.wallets.Wallet;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CoinService {
     private final CoinRepository coinRepo;
+    private List<String> coinPairs;
+    private CoinbaseDAO coinDAO;
     public CoinService(CoinRepository coinRepo){
         this.coinRepo = coinRepo;
+        coinDAO = new CoinbaseDAO();
+        coinPairs = coinDAO.getAllCoins();
     }
 
     @Transactional
@@ -36,8 +44,41 @@ public class CoinService {
         return coinRepo.findCoinsByWalletId(walletId);
     }
 
+    public boolean validBuyAmount(Wallet wallet, CoinPurchaseRequest coinDTO){
+        //check if coin they want to buy exists
+        if(!isCurrencyPairValid(coinDTO.getCurrPair())){
+            throw new NoSuchElementException("currency pair doesn't exist");
+        }
+
+        //check if they're too poor to buy
+        double usdAmount =(coinDTO.getAmount())*(coinDAO.valueOf(coinDTO.getCurrPair()));
+        if(wallet.getCashBalance() < usdAmount ){
+            throw new InsufficientFundsException("get more money and come back");
+        }
+
+        return true;
+    }
+
+    public boolean validSellAmount(){
+        return false;
+    }
+
+    //checks if coin exists in a passed in wallet
+    public boolean existsInWallet(){
+        //TODO: implement me
+        return false;
+    }
 
     //verification methods
-
+    public boolean isCurrencyPairValid(String currPair){
+        return coinPairs.stream().anyMatch(c -> c.equals(currPair));
+//        delete me
+//        for(String pair : coinPairs){
+//            if(currPair.equals(pair)){
+//                return true;
+//            }
+//        }
+//        return false;
+    }
 
 }
