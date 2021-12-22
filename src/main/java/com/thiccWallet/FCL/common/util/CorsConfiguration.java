236cf5a3d336cc72582a65d65014c00ae7b7bd1a
Heather <ada@VIIchariot.com>
2021-12-21 19:44:08 -0600

    Created CorsConfiguration

---
+++ src/main/java/com/thiccWallet/FCL/common/util/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.thiccWallet.FCL.common.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+                .allowedOrigins("http://localhost:3000", "http://fantasycoinleague.club")
+                .allowedMethods("*")
+//                .allowedHeaders("header1", "header2", "header3")
+//                .exposedHeaders("header1", "header2")
+                .allowCredentials(true);//.maxAge(3600);
+    }
+}
