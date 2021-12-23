247228a1e014e4f703979f441d1cee390879a6dc
Andrew Aslakson <andrew01177@gmail.com>
2021-12-23 04:58:11 -0500

    Added 2 tests

--- src/test/java/com/thiccWallet/FCL/data/coin/CoinServiceTest.java
+++ src/test/java/com/thiccWallet/FCL/data/coin/CoinServiceTest.java
@@ -1,10 +1,15 @@
 package com.thiccWallet.FCL.data.coin;
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class CoinServiceTest {
     private CoinRepository mockCoinRepo;
@@ -21,19 +26,58 @@ public class CoinServiceTest {
         mockCoinRepo = mock(CoinRepository.class);
 
         //have to mock getAllCoins in CoinService Constructor
-        List<Coin> mockCurrencyPairList = new ArrayList<>();
-        mockCurrencyPairList.add(new Coin("BTC-USD", 0.0, null));
-        mockCurrencyPairList.add(new Coin("ETH-USD", 0.0, null));
-        mockCurrencyPairList.add(new Coin("USDT-USD", 0.0, null));
+        List<String> mockCurrencyPairList = new ArrayList<>();
+        mockCurrencyPairList.add("BTC-USD");
+        mockCurrencyPairList.add("ETH-USD");
+        mockCurrencyPairList.add("USDT-USD");
 
-        when(mockCoinbaseDAO.getAllCoins()).thenReturn(mockCurrencyPairList);
+        when(mockCoinDAO.getAllCoins()).thenReturn(mockCurrencyPairList);
 
-        sut = new CoinService(mockCoinDAO, mockCoinbaseDAO);
+        sut = new CoinService(mockCoinRepo);
     }
 
     @After
     public void testCaseCleanUp() {
         sut = null;
+    }
+
+    @Test
+    public void test_getPairs_returnsNotNull() {
+
+        // Arrange
+        List<Coin> mockCoinList = new ArrayList<>();
+        mockCoinList.add(new Coin("WalletId", "Curr_pair", 1));
+        mockCoinList.add(new Coin("WalletId", "Curr_pair", 1));
+        mockCoinList.add(new Coin("WalletId", "Curr_pair", 1));
+
+        when(mockCoinRepo.findAll()).thenReturn(mockCoinList);
+
+        // Act
+        List<Coin> actualResult = sut.getPairs();
+
+        // Assert
+        verify(mockCoinRepo, times(1)).findAll();
+        Assert.assertNotNull("Expected List to be returned", actualResult);
+
+    }
+
+    @Test
+    public void test_getCoin_returnsNotNullInOptional_GivenValidCoinId() {
+
+        // Arrange
+        CoinId validCoinId = new CoinId("Curr_pair", "WalletId");
+
+        Optional<Coin> validCoinOptional = Optional.of(new Coin("WalletId", "Curr_pair", 1));
+
+        when(mockCoinRepo.findById(validCoinId)).thenReturn(validCoinOptional);
+
+        // Act
+        Optional<Coin> actualResult = sut.getCoin(validCoinId);
+
+        // Assert
+        verify(mockCoinRepo, times(1)).findById(validCoinId);
+        Assert.assertTrue("Expected Optional with present item", actualResult.isPresent());
+
     }
 
 }
